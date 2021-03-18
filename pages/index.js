7d204cc4e3dc5428aad4eeb5a7cb0831c5f05c48
HS <hsmok81@gmail.com>
2021-03-18 15:28:01 +0700

    nextjs sample

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import Head from 'next/head'
+
+function HomePage(props) {
+    return (
+        <>
+            <Head>
+                <title>Welcome to Next.js!</title>
+            </Head>
+            <div>Welcome to Next.js!</div>
+            <Link href="/posts/first">
+                <a>First Post</a>
+            </Link>
+        </>	    
+    )
+}
+
+export async function getServerSideProps(context) {
+    const res = await fetch('https://api.github.com/repos/vercel/next.js')
+    const json = await res.json()
+    console.log(json)
+    return {
+        props: { stars: json.stargazers_count }
+    }
+}
+
+export default HomePage
---
+++ pages/posts/first.js
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Container from '../../components/container'
+
+export default function FirstPost(props) {
+   return (
+      <>
+         <Container>
+            <Head>
+               <title>My First Post</title>
+            </Head>
+            <h1>My First Post</h1>
+            <h2>
+               <Link href="/">
+                  <a>Home</a>
+               </Link>
+               <div>Next stars: {props.stars}</div>
+            </h2>
+         </Container>
+      </>	  
+   )
+}
+
+export async function getStaticProps() {
+    const res = await fetch('https://api.github.com/repos/vercel/next.js')
+    const json = await res.json()
+    return {
+        props: { stars: json.stargazers_count }
+    }
+}
